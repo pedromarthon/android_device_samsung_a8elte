839a7eed5e8b22629086c6394ea87a56dfbbd269
grace5921 <androidlover5842@gmail.com>
2016-03-23 17:16:28 +0530

    remove un needed files

--- device_a8elte.mk
+++ device_a8elte.mk
@@ -17,7 +17,6 @@ PRODUCT_PACKAGES += \
     fstab.universal5430 \
     init.universal5430.rc \
     init.universal5430.baseband.rc \
-    init.universal5430.power.rc \
     init.universal5430.usb.rc \
     init.universal5430.wifi.rc \
     ueventd.universal5430.rc \
